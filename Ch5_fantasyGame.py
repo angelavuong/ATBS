1a4d77e7db22612790d52712908e1e69bc9aa960
Angela Vuong <angvuong@cisco.com>
2018-02-23 16:08:22 -0500

    Fantasy game

--- Ch5_fantasyGame.py
+++ Ch5_fantasyGame.py
@@ -1,12 +1,22 @@
 # inventory.py
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def displayInventory(inventory):
+    global inv
     print("Inventory:")
     item_total = 0
-    for k, v in inventory.items():
+    for k, v in inv.items():
         print(str(v) + ' ' + str(k))
-        item_total =  + item_total + v
+        item_total =  sum(inv.values())
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+def addToInventory(inventory, addedItems):
+    # your code goes here
+    for i in range(len(addedItems)):
+        inv.setdefault(addedItems[i], 0)
+        inv[addedItems[i]] = inv[addedItems[i]] + 1
+    return inv
+
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
